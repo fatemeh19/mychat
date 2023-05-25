16dd05a2461a423c73680e5893f466d6faa0cfd5
kosar78 <kossarhoseini78@gmail.com>
2023-05-25 12:43:30 +0330

    create sidebar

--- frontend/src/components/leftSideMainPage/index.tsx
+++ frontend/src/components/leftSideMainPage/index.tsx
@@ -1,10 +1,13 @@
 "use client"
+
+import SideBar from "./sideBar"
+
 export default function LeftSideMainPage(){
 
     return (
-        <div className="grid grid-cols-4 h-[450px]">
-            <div className="col-span-1 bg-mainColor">
-                kk
+        <div className="grid grid-cols-4">
+            <div className="col-span-1">
+                <SideBar />
             </div>
             <div className="col-span-3 ">
                 ff
---
+++ frontend/src/components/leftSideMainPage/sidebar/icons/chart.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { BiChart } from "react-icons/bi";
+export default function ChartIcon(){
+
+    return (
+        
+        <div className="flex justify-center py-5">
+            <BiChart className="text-white cursor-pointer opacity-[.5] w-[30px] h-[30px]" />
+        </div>
+        
+    )
+}
