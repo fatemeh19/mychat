28b4c8848f3531d70f354cf1dca7fbc5e7551078
Fatemeh <fatemehveysi1378@gmail.com>
2023-05-28 16:07:23 +0430

    refactor the errorhandler validation message part

--- server/middlewares/error-handler.js
+++ server/middlewares/error-handler.js
@@ -4,8 +4,16 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong please try later",
   };
-  console.log(err)
-
+  
+  console.log(err._message)
+ 
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) =>item.path+':'+item.message)
+      .join(',');
+    customError.statusCode = 400;
+  }
+  
   return res.status(customError.statusCode).json({msg:customError.msg});
 };
 
