f1563191e260992c202a0ade5617b3a86a8e1548
kosar78 <kossarhoseini78@gmail.com>
2023-06-19 10:21:57 +0330

    contact Image : change src

--- frontend/src/components/contact/contactList/contactBox.tsx
+++ frontend/src/components/contact/contactList/contactBox.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image"
 import { type } from "os";
-import { FC ,createRef} from "react"
+import { FC } from "react"
 type Contact={
     name: string;
     _id: string;
@@ -15,12 +15,14 @@ interface ContactBoxProps {
 const ContactBox: FC<ContactBoxProps> = ({
     contact
 }) => {
-    console.log(contact)
-    const profileImg = createRef<HTMLImageElement>()
+    
+    const profilePicName=(contact.profilePic).split(`\\`)
+    console.log(profilePicName[profilePicName.length-1])
+
     
     return (
                     <div key={contact._id} className='w-full flex gap-2   mt-3'>
-                        <img ref={profileImg} src={contact.profilePic} className="w-[50px] h-[50px] rounded-full " width={50} height={50} alt="contact-profile" />
+                        <Image src={`/uploads/${profilePicName}`} className="w-[50px] h-[50px] rounded-full " width={50} height={50} alt="contact-profile" />
                         <div className="contact-details h-full pt-1 gap-1 grid w-full">
                             <p className="contact-name font-bold text-sm">{contact.name}</p>
                             <p className="status text-xs text-sky-500">Online</p>
