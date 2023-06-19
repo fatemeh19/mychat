8466f2b48a8ddd5938f9280b8736f60191c91a1c
kosar78 <kossarhoseini78@gmail.com>
2023-06-19 11:26:13 +0330

    contact image :change src 2

--- frontend/src/components/contact/contactList/contactBox.tsx
+++ frontend/src/components/contact/contactList/contactBox.tsx
@@ -16,13 +16,13 @@ const ContactBox: FC<ContactBoxProps> = ({
     contact
 }) => {
     
-    const profilePicName=(contact.profilePic).split(`\\`)
-    console.log(profilePicName[profilePicName.length-1])
-
+    const profilePicName=(contact.profilePic).split(`\\`);
     
     return (
                     <div key={contact._id} className='w-full flex gap-2   mt-3'>
-                        <Image src={`/uploads/${profilePicName}`} className="w-[50px] h-[50px] rounded-full " width={50} height={50} alt="contact-profile" />
+                        <Image src={`/uploads/${profilePicName[profilePicName.length-1]}`} 
+                            className="w-[50px] h-[50px] object-cover rounded-full " 
+                            width={50} height={50} alt="contact-profile" />
                         <div className="contact-details h-full pt-1 gap-1 grid w-full">
                             <p className="contact-name font-bold text-sm">{contact.name}</p>
                             <p className="status text-xs text-sky-500">Online</p>
