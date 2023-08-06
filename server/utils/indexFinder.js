4beca99db7b270d9e88ebdc92a878c9cb9764f2a
Fatemeh <fatemehveysi1378@gmail.com>
2023-08-06 12:38:34 +0330

    extra logs removed

--- server/utils/indexFinder.js
+++ server/utils/indexFinder.js
@@ -1,9 +1,6 @@
 const indexFinder =  (x,y,list,value,reverse=false)=>{
     let borderIndex = Math.floor((x+y)/2)
-    // console.log(x, y , list , value)
-    console.log("list=",list)
-    console.log("value=",value)
-    console.log("list[border]=",list[borderIndex])
+   
     if(list[borderIndex].equals(value)){
         return borderIndex
     }else{
