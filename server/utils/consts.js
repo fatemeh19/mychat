fe60869ed6d72800eb1555a1ae93987510d35514
Fatemeh <fatemehveysi1378@gmail.com>
2023-08-12 14:00:49 +0330

    create default system file and set new path

--- server/utils/consts.js
+++ server/utils/consts.js
@@ -2,4 +2,5 @@ import path,{dirname} from 'path'
 import { fileURLToPath } from "url";
 
 export const MEDIA_SAVE_PATH = "../frontend/public/uploads/"
-export const DEFAULT_PROFILE_PICTURE = '..\\frontend\\public\\uploads\\picture\\defaultProfilePic.png'
+export const DEFAULT_PROFILE_PICTURE = '..\\frontend\\public\\defaults\\defaultProfilePic.jpg'
+export const DEFAULT_NOTIF_SOUND = '..\\frontend\\public\\defaults\\defaultNotification.mp3'
