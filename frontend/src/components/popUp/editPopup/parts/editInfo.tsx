fc819aa5747eaf5347d2233aa2436bce306f7389
fatemehAhmadian <ahmadian.fatemeh.00@gmail.com>
2023-08-19 14:17:21 +0330

    design description in edit chatInfo

--- frontend/src/components/popUp/editPopup/parts/editInfo.tsx
+++ frontend/src/components/popUp/editPopup/parts/editInfo.tsx
@@ -6,6 +6,8 @@ import { setIsEditChat } from "@/src/redux/features/chatSlice";
 import ProfileImgSelector from "@/src/components/completeInformationForm/profileImgSelector";
 import InputField from "@/src/components/auth/input/inputField";
 import PopUpBtns from "../../popUpBtns";
+import Image from "next/image";
+import { TextareaAutosize } from "@mui/material";
 
 
 interface EditChatInfoProps {
@@ -20,6 +22,7 @@ const EditChatInfo: FC<EditChatInfoProps> = ({ setFormikbtnRef, setImage, chatNa
 
     const chatProfilePic = useAppSelector(state => state.chat).Chat.profilePic.split('\\')
     const [chatProfilePicName, setChatProfilePicName] = useState('')
+    const [chatDescription, setChatDescription] = useState('')
     const formikbtnRef = useRef<HTMLButtonElement>(null)
 
     useEffect(() => {
@@ -28,7 +31,8 @@ const EditChatInfo: FC<EditChatInfoProps> = ({ setFormikbtnRef, setImage, chatNa
     }, [])
 
     const initialValues = {
-        chatName: chatName
+        chatName: chatName,
+        description: ''
     }
 
     const validation = yup.object().shape({
@@ -46,9 +50,44 @@ const EditChatInfo: FC<EditChatInfoProps> = ({ setFormikbtnRef, setImage, chatNa
                 <Form >
                     <div className="overflow-hidden w-full flex flex-col items-end gap-5 relative select-none p-[25px] bg-white
                                 shadow-[0_3px_3px_-2px_rgb(0,0,0,0.1)]">
-                        <div className="no-scrollbar flex overflow-y-auto w-full ">
-                            <ProfileImgSelector chatProfilePicName={chatProfilePicName} setImage={setImage} styleClassName={'!w-24 !h-24'} />
-                            <InputField name="chatName" label="chat name" containerClassName={'w-full !mr-0'} />
+                        <div className="no-scrollbar flex flex-col overflow-y-auto w-full ">
+                            <div className="flex w-full">
+                                <ProfileImgSelector chatProfilePicName={chatProfilePicName} setImage={setImage} styleClassName={'!w-24 !h-24'} />
+                                {/* <Image
+                                    width={500}
+                                    height={0}
+                                    src={
+                                        contact.profilePic
+                                            ? `/uploads/photo/${profilePicName[profilePicName.length - 1]}`
+                                            : '/uploads/photo/defaultProfilePic.png'
+                                    }
+                                    alt=""
+                                    className="w-[70px] h-[70px] object-cover rounded-full"
+                                /> */}
+                                <InputField name="chatName" label="chat name" containerClassName={'w-full !mr-0'} />
+                            </div>
+                            {/* <TextareaAutosize /> 
+                             */}
+                            <TextareaAutosize
+                                placeholder="Description (optional)"
+                                className="
+                                    pt-6
+                                    w-full 
+                                    bg-transparent
+                                    text-[14px]
+                                    font-[200]
+                                    placeholder:text-sm
+                                    focus-visible:outline-none
+                                    resize-none
+                                    placeholder:leading-[1.8rem]
+                                    leading-5
+                                    untvisible-scrollbar
+                                    font-[Arial,FontAwesome]
+                                "
+                                maxRows={12}
+                                onChange={e => setChatDescription(e.target.value)}
+                                value={chatDescription}
+                            />
                         </div>
                     </div>
                     <button type="submit" ref={formikbtnRef} hidden ></button>
