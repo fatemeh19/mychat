84e5928da3b7e16fe2450caf2a58a133c00ad79f
fatemehAhmadian <ahmadian.fatemeh.00@gmail.com>
2023-08-21 15:30:06 +0330

    profilepic type chang to profilePicInterface

--- frontend/src/redux/features/userChatListSlice.ts
+++ frontend/src/redux/features/userChatListSlice.ts
@@ -1,8 +1,9 @@
+import { profilePicInterface } from "@/src/models/interface";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 interface chatInfoInterface {
     _id: string,
     name: string,
-    profilePic: string,
+    profilePic: profilePicInterface,
     status: {
         online: boolean,
         lastseen: string | Date | number
