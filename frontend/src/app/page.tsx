e8b54b33c0b9ea6998d7a95d8545a7d4cab3d35f
fatemehAhmadian <ahmadian.fatemeh.00@gmail.com>
2023-08-29 16:00:03 +0330

    render login in home link

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,10 @@
-import Register from './auth/register/page'
+import Login from './auth/login/page'
 
 export default function MainPage() {
 
   return (
     <main >
-      <Register />
+      <Login />
     </main>
   )
 }
